66f62a99ce6018bd9083ec4705aa5e064ec56b1b
LordMike <lordmike@iki.fi>
2016-07-05 05:54:03 +0300

    simple cuda add test

---
+++ cudaAdd.cuh
@@ -0,0 +1,7 @@
+#ifndef CUDAADD_CUH
+#define CUDAADD_CUH
+
+// Forward declare the function in the .cu file
+void vectorAddition(const float* a, const float* b, float* c, int n);
+
+#endif // CUDAADD_CUH
